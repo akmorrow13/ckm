2f14b54426c25036cdd36da7853fdc82b9158c1d
Vaishaal Shankar <vaishaal@berkeley.edu>
2016-03-11 16:30:26 -0800

    added a sweeper script

--- sweep.py
+++ sweep.py
@@ -3,20 +3,23 @@ import pickle
 import yaml
 
 base_exp =  load(open("./keystone_pipeline/cifar_cluster.exp"))
+base_exp['expid'] = "4x4x128_gamma_reg_sweep"
 # 1 Layer sweep
 results = []
-for bw1 in [0.5, 0.7, 1, 1.2]:
-    for bw in [1e-6, 1e-5, 1e-4, 1e-3, 0.01, 0.1, 0.5,0.7,2, 5 ,10, 20, 100]:
+for gamma in [1e-6, 5e-6, 1e-7]:
+    for reg in [1e-3, 5e-3, 1e-4]:
         exp_copy = base_exp.copy()
-        exp_copy['bandwidth'][-1] = bw
-        exp_copy['bandwidth'][0] = bw1
-        fname = "/tmp/bw_sweep_{0}".format(bw)
+        exp_copy["kernelGamma"] = gamma
+        fname = "/tmp/gamma_reg_sweep_{0}_{1}".format(gamma,  reg)
         sweep_file = open(fname, "w+")
         yaml.dump(exp_copy, sweep_file)
         sweep_file.close()
         result = scala_run(exp_copy, fname)
         results.append(result)
 
-for r in results:
-    print tabulate(r, headers="keys")
-    print "===================================="
+for i,r in enumerate(results):
+    if (i == 0):
+        header = True
+    else:
+        header = False
+    print r.to_csv(header=header)
